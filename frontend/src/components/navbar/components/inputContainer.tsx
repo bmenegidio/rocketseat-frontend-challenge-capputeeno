56819db874a5377c6de6919067ead5a625c538e2
Bruno Menegidio <bruno.silva@conecta.tech>
2023-07-17 15:46:32 -0300

    feat: navbar input

---
+++ frontend/src/components/navbar/components/inputContainer.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const InputContainer = styled.div`
+  position: relative;
+  width: 22rem;
+
+  svg {
+    position: absolute;
+    right: 1rem;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+`;
--- frontend/src/components/navbar/index.tsx
+++ frontend/src/components/navbar/index.tsx
@@ -1,12 +1,22 @@
 'use client';
 
+import { SearchIcon } from '@/components/icons/search';
+import { Input } from '@/components/input';
 import { CartControl } from '@/components/navbar/components/cartControl';
 import { Header } from '@/components/navbar/components/header';
+import { InputContainer } from '@/components/navbar/components/inputContainer';
 import { Logo } from '@/components/navbar/components/logo';
+import { NavRightContainer } from '@/components/navbar/components/navRightContainer';
 
 export const Navbar = () => (
   <Header>
     <Logo />
-    <CartControl />
+    <NavRightContainer>
+      <InputContainer>
+        <Input placeholder='Procurando por algo específico?' />
+        <SearchIcon />
+      </InputContainer>
+      <CartControl />
+    </NavRightContainer>
   </Header>
 );
