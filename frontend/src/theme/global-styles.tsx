5d3d8c9eec485295d021e379abf1932a57c786f1
Bruno Menegidio <bruno.silva@conecta.tech>
2023-07-17 15:39:32 -0300

    style: default font styles for elements

--- frontend/src/theme/global-styles.tsx
+++ frontend/src/theme/global-styles.tsx
@@ -5,9 +5,6 @@ const saira = Saira({ subsets: ['latin'], weight: ['400', '500', '600'] });
 
 export const GlobalStyles = createGlobalStyle`
   body {
-    font-family: ${saira.style.fontFamily};
-    font-style: ${saira.style.fontStyle};
-    font-weight: 400;
     font-size: 16px;
     background: ${(props) => props.theme.colors.bgPrimary};
   }
@@ -16,5 +13,8 @@ export const GlobalStyles = createGlobalStyle`
     box-sizing: border-box;
     margin: 0;
     padding: 0;
+    font-family: ${saira.style.fontFamily};
+    font-style: ${saira.style.fontStyle};
+    font-weight: 400;
   }
 `;
