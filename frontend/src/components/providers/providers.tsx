4a57c99b601c7ac348dd2235a9cdc85590905d0e
Bruno Menegidio <bruno.silva@conecta.tech>
2023-07-18 17:11:50 -0300

    feat: create appolo instance & add provider

--- frontend/src/components/providers/providers.tsx
+++ frontend/src/components/providers/providers.tsx
@@ -1,23 +1,27 @@
 'use client';
 
+import { ApolloProvider } from '@apollo/client';
 import { ReactNode } from 'react';
 import { ThemeProvider } from 'styled-components';
 
 import StyledComponentsRegistry from '@/components/providers/styled-components-registry';
 import { CartProvider } from '@/contexts/cartContext/cartContext';
 import { FilterProvider } from '@/contexts/filterContext/filterContext';
+import { apiClient } from '@/services/api';
 import { theme } from '@/theme';
 import { GlobalStyles } from '@/theme/global-styles';
 
 export const Providers = ({ children }: { children: ReactNode }) => (
   <StyledComponentsRegistry>
     <ThemeProvider theme={theme}>
-      <FilterProvider>
-        <CartProvider>
-          <GlobalStyles />
-          {children}
-        </CartProvider>
-      </FilterProvider>
+      <ApolloProvider client={apiClient}>
+        <FilterProvider>
+          <CartProvider>
+            <GlobalStyles />
+            {children}
+          </CartProvider>
+        </FilterProvider>
+      </ApolloProvider>
     </ThemeProvider>
   </StyledComponentsRegistry>
 );
