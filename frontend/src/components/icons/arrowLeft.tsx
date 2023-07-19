f650ea3ef726d26721aa8f1475717d9d62dcfb63
Bruno Menegidio <bruno.silva@conecta.tech>
2023-07-19 10:50:04 -0300

    feat: create arrowLeftIcon component

---
+++ frontend/src/components/icons/arrowLeft.tsx
@@ -0,0 +1,17 @@
+export const ArrowLeftIcon = () => (
+  <svg
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+    fill='none'
+    xmlns='http://www.w3.org/2000/svg'
+  >
+    <path
+      d='M14 16L10 12L14 8'
+      stroke='#737380'
+      strokeWidth='1.5'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+  </svg>
+);
