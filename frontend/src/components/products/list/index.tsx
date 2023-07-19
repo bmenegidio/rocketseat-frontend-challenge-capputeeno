a5a15c6c2731fed8d428304aca36e69340f32b14
Bruno Menegidio <bruno.silva@conecta.tech>
2023-07-19 11:49:29 -0300

    feat: include pagination in product list

--- frontend/src/components/products/list/index.tsx
+++ frontend/src/components/products/list/index.tsx
@@ -1,15 +1,18 @@
 import { Divider } from '@/components/divider';
+import { Pagination } from '@/components/pagination';
 import { FooterContainerStyled } from '@/components/products/list/components/footerContainerStyled';
 import { ImageStyled } from '@/components/products/list/components/imageStyled';
 import { ListItemStyled } from '@/components/products/list/components/listItemStyled';
 import { ProductsListStyled } from '@/components/products/list/components/listStyled';
 import { ProductNameStyled } from '@/components/products/list/components/productNameStyled';
 import { ProductPriceStyled } from '@/components/products/list/components/productPriceStyled';
+import { useFilter } from '@/contexts/filterContext/filterContext';
 import { useProducts } from '@/hooks/useProducts';
 import { convertPriceInCentsToBrl } from '@/utils/converters';
 
 export const ProductsList = () => {
-  const { products } = useProducts();
+  const { products, totalProducts, loading } = useProducts();
+  const { currentPage, setPage, perPage } = useFilter();
 
   const renderItem = (product: any) => (
     <ListItemStyled key={product.id}>
@@ -24,5 +27,19 @@ export const ProductsList = () => {
     </ListItemStyled>
   );
 
-  return <ProductsListStyled>{products.map(renderItem)}</ProductsListStyled>;
+  return (
+    <>
+      {!loading && (
+        <>
+          <Pagination
+            totalItems={totalProducts}
+            currentPage={currentPage}
+            onPageChange={setPage}
+            perPage={perPage}
+          />
+          <ProductsListStyled>{products.map(renderItem)}</ProductsListStyled>
+        </>
+      )}
+    </>
+  );
 };
