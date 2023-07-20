0c36f8acd0e32c533f369d32d803e6f5415a7255
Bruno Menegidio <bruno.silva@conecta.tech>
2023-07-20 17:35:20 -0300

    refactor: move cart items component

--- frontend/src/app/cart/page.tsx
+++ frontend/src/app/cart/page.tsx
@@ -1,102 +1,15 @@
 'use client';
 
-import { BackButton } from '@/components/backButton';
-import { Button } from '@/components/cart/cartItems/button';
-import { CardContainer } from '@/components/cart/cartItems/cardContainer';
-import { CardContent } from '@/components/cart/cartItems/cardContent';
-import { CardDescription } from '@/components/cart/cartItems/cardDescription';
-import { CardtItem } from '@/components/cart/cartItems/cardtItem';
-import { CardtItemsContainer } from '@/components/cart/cartItems/cardtItemsContainer';
-import { CardTitle } from '@/components/cart/cartItems/cardTitle';
+import { CartItems } from '@/components/cart/cartItems';
 import { ContentFlex } from '@/components/cart/cartItems/contentFlex';
-import { Image } from '@/components/cart/cartItems/image';
-import { ImageContainer } from '@/components/cart/cartItems/imageContainer';
-import { ItemPrice } from '@/components/cart/cartItems/itemPrice';
-import { Select } from '@/components/cart/cartItems/select';
-import { SubTitle, SubTitleStrong } from '@/components/cart/cartItems/subTitle';
-import { Title } from '@/components/cart/cartItems/title';
 import { OrderSummary } from '@/components/cart/orderSummary';
-import { TrashIcon } from '@/components/icons/trash';
 import { PageContentContainer } from '@/components/pageContentContainer';
-import {
-  MAX_PRODUCTS_QUANTITY_PER_CART,
-  useCartContext,
-} from '@/contexts/cartContext/cartContext';
-import { UpdateProductQuantityProps } from '@/contexts/cartContext/models/updateProductQuantityProps';
 
 export default function CartPage() {
-  const {
-    cartItems,
-    subTotalProductsInBrl,
-    updateProductQuantity,
-    deleteProduct,
-  } = useCartContext();
-
-  function handleChangeProductQuantity({
-    productId,
-    quantity,
-  }: UpdateProductQuantityProps) {
-    updateProductQuantity({
-      productId,
-      quantity,
-    });
-  }
-
   return (
     <PageContentContainer>
       <ContentFlex>
-        <CardtItemsContainer>
-          <BackButton route={'/products'} />
-          <Title>Seu Carrinho</Title>
-          <SubTitle>
-            Total ({cartItems.length} produtos){' '}
-            <SubTitleStrong>{subTotalProductsInBrl}</SubTitleStrong>
-          </SubTitle>
-
-          <CardContainer>
-            {cartItems.map((item) => (
-              <CardtItem key={item.id}>
-                <ImageContainer>
-                  <Image src={item.image_url} alt={item.name} />
-                </ImageContainer>
-                <CardContent>
-                  <div>
-                    <div className='row'>
-                      <CardTitle>{item.name}</CardTitle>
-                      <Button onClick={() => deleteProduct(item.id)}>
-                        <TrashIcon />
-                      </Button>
-                    </div>
-                    <CardDescription>{item.description}</CardDescription>
-                  </div>
-
-                  <div className='row'>
-                    <Select
-                      value={item.quantity}
-                      onChange={(event) =>
-                        handleChangeProductQuantity({
-                          productId: item.id,
-                          quantity: Number(event.target.value),
-                        })
-                      }
-                    >
-                      {Array.from(
-                        { length: MAX_PRODUCTS_QUANTITY_PER_CART },
-                        (_, index) => index + 1,
-                      ).map((quantity) => (
-                        <option key={quantity} value={quantity}>
-                          {quantity}
-                        </option>
-                      ))}
-                    </Select>
-                    <ItemPrice>{item.priceInBrl}</ItemPrice>
-                  </div>
-                </CardContent>
-              </CardtItem>
-            ))}
-          </CardContainer>
-        </CardtItemsContainer>
-
+        <CartItems />
         <OrderSummary />
       </ContentFlex>
     </PageContentContainer>
---
+++ frontend/src/components/cart/cartItems/index.tsx
@@ -0,0 +1,93 @@
+import { BackButton } from '@/components/backButton';
+import { Button } from '@/components/cart/cartItems/button';
+import { CardContainer } from '@/components/cart/cartItems/cardContainer';
+import { CardContent } from '@/components/cart/cartItems/cardContent';
+import { CardDescription } from '@/components/cart/cartItems/cardDescription';
+import { CardtItem } from '@/components/cart/cartItems/cardtItem';
+import { CardtItemsContainer } from '@/components/cart/cartItems/cardtItemsContainer';
+import { CardTitle } from '@/components/cart/cartItems/cardTitle';
+import { Image } from '@/components/cart/cartItems/image';
+import { ImageContainer } from '@/components/cart/cartItems/imageContainer';
+import { ItemPrice } from '@/components/cart/cartItems/itemPrice';
+import { Select } from '@/components/cart/cartItems/select';
+import { SubTitle, SubTitleStrong } from '@/components/cart/cartItems/subTitle';
+import { Title } from '@/components/cart/cartItems/title';
+import { TrashIcon } from '@/components/icons/trash';
+import {
+  MAX_PRODUCTS_QUANTITY_PER_CART,
+  useCartContext,
+} from '@/contexts/cartContext/cartContext';
+import { UpdateProductQuantityProps } from '@/contexts/cartContext/models/updateProductQuantityProps';
+
+export const CartItems = () => {
+  const {
+    cartItems,
+    subTotalProductsInBrl,
+    updateProductQuantity,
+    deleteProduct,
+  } = useCartContext();
+
+  function handleChangeProductQuantity({
+    productId,
+    quantity,
+  }: UpdateProductQuantityProps) {
+    updateProductQuantity({
+      productId,
+      quantity,
+    });
+  }
+
+  return (
+    <CardtItemsContainer>
+      <BackButton route={'/products'} />
+      <Title>Seu Carrinho</Title>
+      <SubTitle>
+        Total ({cartItems.length} produtos){' '}
+        <SubTitleStrong>{subTotalProductsInBrl}</SubTitleStrong>
+      </SubTitle>
+
+      <CardContainer>
+        {cartItems.map((item) => (
+          <CardtItem key={item.id}>
+            <ImageContainer>
+              <Image src={item.image_url} alt={item.name} />
+            </ImageContainer>
+            <CardContent>
+              <div>
+                <div className='row'>
+                  <CardTitle>{item.name}</CardTitle>
+                  <Button onClick={() => deleteProduct(item.id)}>
+                    <TrashIcon />
+                  </Button>
+                </div>
+                <CardDescription>{item.description}</CardDescription>
+              </div>
+
+              <div className='row'>
+                <Select
+                  value={item.quantity}
+                  onChange={(event) =>
+                    handleChangeProductQuantity({
+                      productId: item.id,
+                      quantity: Number(event.target.value),
+                    })
+                  }
+                >
+                  {Array.from(
+                    { length: MAX_PRODUCTS_QUANTITY_PER_CART },
+                    (_, index) => index + 1,
+                  ).map((quantity) => (
+                    <option key={quantity} value={quantity}>
+                      {quantity}
+                    </option>
+                  ))}
+                </Select>
+                <ItemPrice>{item.priceInBrl}</ItemPrice>
+              </div>
+            </CardContent>
+          </CardtItem>
+        ))}
+      </CardContainer>
+    </CardtItemsContainer>
+  );
+};
