ea976a84b5edca562175e009ed3b9454e84311b7
Bruno Menegidio <bruno.silva@conecta.tech>
2023-07-20 15:26:49 -0300

    feat: navigate to cart on header cart icon click

--- frontend/src/components/navbar/components/cartControl.tsx
+++ frontend/src/components/navbar/components/cartControl.tsx
@@ -1,3 +1,5 @@
+import { useRouter } from 'next/navigation';
+
 import { ShoppingBagIcon } from '@/components/icons/shoppingBag';
 import { CartBadge } from '@/components/navbar/components/cartBadge';
 import { CartContainer } from '@/components/navbar/components/cartContainer';
@@ -5,8 +7,14 @@ import { useCartContext } from '@/contexts/cartContext/cartContext';
 
 export function CartControl() {
   const { cartItems } = useCartContext();
+  const router = useRouter();
+
+  function handleNavigateToCart() {
+    router.push('/cart');
+  }
+
   return (
-    <CartContainer>
+    <CartContainer onClick={handleNavigateToCart}>
       <ShoppingBagIcon />
       {cartItems.length > 0 && <CartBadge>{cartItems.length}</CartBadge>}
     </CartContainer>
